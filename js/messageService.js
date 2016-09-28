ea1ba5077c23081f850804c48cf425d3a52c8c05
Jacob <admin@Mjolnir.local>
2016-09-28 15:55:47 -0600

    updates

--- js/messageService.js
+++ js/messageService.js
@@ -1,14 +1,15 @@
-angular.module('chatroom').service('messageService', function($http){
+angular.module('chatroom').service('messageService',
+function($http){
   //Here you'll need to create two methods. One called postMessage and the other called
   //getMessages.
+
+
 
   //On the lines below create a getMessages method. This method will retrieve data
   // from the backend.
   //The url for the get request should be 'http://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your
   //controller.
-
-
 
   //On the line below create the postMessage method. This method will add data to the
   //backend server.
@@ -22,8 +23,40 @@ angular.module('chatroom').service('messageService', function($http){
   //(return $http(...)), you can then use .then in your controller.
 
   //postMessage method here
-
+  this.postMessage = function(yourMessage){
+    return $http({
+    method: 'POST',
+    url:'http://practiceapi.devmounta.in/api/chats',
+    data: {message: yourMessage}
+    });
+  }
 
   //getMessages method here
+  this.getMessages = function(){
+    return $http({
+    method: 'GET',
+    url:'http://practiceapi.devmounta.in/api/chats'
+    });
+
+  }
+
+  // postCookie!
+  this.postCookie = function(cookies){
+    return $http({
+    method: 'POST',
+    url:'http://practiceapi.devmounta.in/api/cookies',
+    data: {cookie: cookies}
+    });
+  }
+
+  //getCookies!
+  this.getCookies = function(){
+    return $http({
+    method: 'GET',
+    url:'http://practiceapi.devmounta.in/api/cookies'
+    });
+
+  }
+
 
 });
